d53b0ff14e7f87159259d612d7b7035b267221b1
dia2309 <diana.maria.bogdan@stud.ubbcluj.ro>
2026-05-22 08:59:16 +0300

    Save minor change

--- backend/src/main/java/integra/backend/event/service/EventService.java
+++ backend/src/main/java/integra/backend/event/service/EventService.java
@@ -20,29 +20,29 @@ public class EventService implements IEventService {
             new RegisteredVolunteerDto(9L, "Ana", "Blandiana", "ana.b@lit.ro"),
             new RegisteredVolunteerDto(10L, "George", "Enescu", "george.e@music.ro"),
             new RegisteredVolunteerDto(11L, "Victor", "Breb", "victor.b@tech.com"),
-            new RegisteredVolunteerDto(12L, "Laura", "Codruța", "laura.c@justice.ro")
+            new RegisteredVolunteerDto(12L, "Laura", "Codruța", "laura.c@justice.ro"),
+            new RegisteredVolunteerDto(14L, "Victoria", "Georgescu", "victoria.geo@gmail.ro"),
+            new RegisteredVolunteerDto(16L, "Marius", "Avram", "marius.avram@gmail.com")
+
+
         );
 
         List<RegisteredVolunteerDto> eventSpecificList = mockData.stream()
         .filter(v -> (eventId % 2 == 0) ? (v.id() % 2 == 0) : (v.id() % 2 != 0))
         .toList();
 
-        // If search is empty or too short, return everyone
         if (search == null || search.trim().length() < 3) {
             return eventSpecificList;
         }
 
-        // Filter by First Name, Last Name, or Email (case-insensitive)
         String query = search.trim().replaceAll("\\s+", " ").toLowerCase();
         return eventSpecificList.stream()
             .filter(v -> {
-            // Create a temporary "Full Name" string to compare against
             String firstName = v.firstName().toLowerCase();
             String lastName = v.lastName().toLowerCase();
             String fullName = firstName + " " + lastName;
             String email = v.email().toLowerCase();
 
-            // Check if the query matches ANY of these
             boolean matchName = firstName.contains(query) || lastName.contains(query);
             boolean matchFullName = fullName.contains(query);
             boolean matchEmail = email.contains(query);
