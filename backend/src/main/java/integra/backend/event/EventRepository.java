6e0b6cbe64e9193c4068bb4b0de3e20a1872b4d6
Anado17 <ana.dobra@stud.ubbcluj.ro>
2026-06-05 12:23:14 +0300

    adding missing repo

--- backend/src/main/java/integra/backend/event/EventRepository.java
+++ backend/src/main/java/integra/backend/event/EventRepository.java
@@ -1,9 +1,9 @@
 package integra.backend.event;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import integra.backend.event.model.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-
+    List<Event> findAllByOwnerId(Long ownerId);
 }
